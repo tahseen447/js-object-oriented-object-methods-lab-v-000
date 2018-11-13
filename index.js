4991180349307021ff50ee3ae1a0bc06244d84f6
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-13 06:37:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ function BoardMember(name, homeState, training){
   this.training = training;
 
   this.veto = function(){
-    return "No, I must disagree.";
+    return "No, I must disagree";
   }
   this.approve = function(){
-    return "You can do that!"
+    return "You can do that!";
   }
 }
